e2c4f6f217812f3fc6ce85cc5f45a534074c68c2
aziz <a@gmail.com>
2022-10-17 23:20:22 +0500

    project

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const data = ['Иван', 'Иванов', 'Отдел разработки', 'Программист', 2000];
-let [data1, data2, data3, data4, data5] = data;
-console.log(data1, data2, data3);
+
+function func(emp) {
+    let [data1, data2, data3, data4, data5] = emp;
+    console.log(data1, data2, data3);
+}
+func(data);
